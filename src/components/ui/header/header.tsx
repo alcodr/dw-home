d40c995bcf1190c62c36724873f34a80cfae953a
alcodr <mfrsosmed@gmail.com>
2025-03-04 19:46:53 +0700

    dark mode on header

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -8,20 +8,21 @@ const Header = () => {
 
     const toggleTheme = useThemeStore((state: any) => state.toggleTheme)
     const theme = useThemeStore((state: any) => state.theme)
+    const isDark = theme === 'dark' ? true : false
     useTheme()
 
     return (<>
-        <div className="py-6 px-6 flex justify-between bg-transparent text-white items-center fixed w-100 left-0 right-0 z-[200]" >
+        <div className="py-6 px-6 flex justify-between bg-transparent hover:bg-[#f5f5f5] dark:hover:bg-[#0d0d0d] text-white items-center fixed w-100 left-0 right-0 z-[200]" >
             <div>
-                <TextAlignLeftIcon width="24" height="24"></TextAlignLeftIcon>
+                <TextAlignLeftIcon color={!isDark ? 'black' : 'white'} width="24" height="24"></TextAlignLeftIcon>
             </div>
             <img src={dwLogo} width={150} />
             <div className='flex'>
                 <div className='mr-4' onClick={() => toggleTheme()}>
-                    {theme === 'light' && <SunIcon width="24" height="24"></SunIcon>}
-                    {theme !== 'light' && <MoonIcon width="24" height="24"></MoonIcon>}
+                    {!isDark && <SunIcon color='black' width="24" height="24"></SunIcon>}
+                    {isDark && <MoonIcon color='white' width="24" height="24"></MoonIcon>}
                 </div>
-                <div><PersonIcon width="24" height="24"></PersonIcon></div>
+                <div><PersonIcon color={!isDark ? 'black' : 'white'} width="24" height="24"></PersonIcon></div>
             </div>
         </div>
     </>)
