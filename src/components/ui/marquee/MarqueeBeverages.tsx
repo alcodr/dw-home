90508c0a1ba5fc1c89b36e56197f8118db23b81a
alcodr <mfrsosmed@gmail.com>
2025-03-06 01:18:10 +0700

    adjust marquee beverage background color

--- src/components/ui/marquee/MarqueeBeverages.tsx
+++ src/components/ui/marquee/MarqueeBeverages.tsx
@@ -4,7 +4,7 @@ import beverages from '@/assets/beverages.png'
 const MarqueeBeverages = () => {
     return (
         <div className="absolute z-[-10] top-0 left-0">
-            <Marquee autoFill speed={20} className="bg-black text-white">
+            <Marquee autoFill speed={20} className="dark:bg-[#0d0d0d] bg-[#f5f5f5]  text-[#333333] dark:text-[#DEDEDE]">
                 <img src={beverages} />
             </Marquee>
         </div>
